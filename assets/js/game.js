429cd57c410bbbd2e52bf43b03b9b8507fe625fb
Ibrahim Yunus <younis.ib@gmail.com>
2019-07-09 17:49:53 -0400

    Testing VS 19

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,23 @@
-document.getElementById("wins").innerHTML = 5;
-document.getElementById("losses").innerHTML = 0;
-document.getElementById("guessesLeft").innerHTML = 9;
-document.getElementById("guessed").innerHTML = "a";
+// Creating variables that will hold the number of wins, losses and remaining guesses
+let wins = 0;
+let losses = 0;
+let guessesLeft = 9;
+let userGuess;
+
+// Create an array that lists out all of the options for the computers to guess from
+let computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Create variables that holds references 
+let winsText = document.getElementById("wins");
+let lossesText = document.getElementById("losses");
+let guessesLeftText = document.getElementById("guessesLeft");
+let guessesText = document.getElementById("guessed");
+
+// This function run when the user press a key
+document.onkeyup = function(event) {
+
+ 
+
+
+    
+};

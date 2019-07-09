cba05cf583135787a46da5724ee0b37e4ab7ea80
Ibrahim Yunus <younis.ib@gmail.com>
2019-07-09 02:24:54 -0400

    Initial Upload

---
+++ assets/js/game.js
@@ -0,0 +1,4 @@
+document.getElementById("wins").innerHTML = 5;
+document.getElementById("losses").innerHTML = 0;
+document.getElementById("guessesLeft").innerHTML = 9;
+document.getElementById("guessed").innerHTML = "a";
